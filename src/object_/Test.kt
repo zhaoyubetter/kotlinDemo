4c04e87350b37a68b048b431389e0284589bda13
better <zhaoyubetter@126.com>
2017-06-02 17:11:59 +0800

    object 单例形式

---
+++ src/object_/Test.kt
@@ -0,0 +1,32 @@
+package object_
+
+/**
+ * 对单例模式的支持
+ * Created by zhaoyu1 on 2017/6/2.
+ */
+object Payroll {
+    val allEmployees = arrayListOf<Person>()
+
+    fun calculateSalary() {
+        for (p in allEmployees) {
+
+        }
+    }
+}
+
+class Person
+
+// 单例的做法
+object CaseInsensitiveComparator : Comparator<String> {
+    override fun compare(o1: String, o2: String): Int {
+        return o1.compareTo(o2, ignoreCase = true)
+    }
+}
+
+fun main(args: Array<String>) {
+    println(CaseInsensitiveComparator.compare("abc", "ABC"))
+
+    val list = listOf<String>("a","d", "b","g", "c")
+     println(list.sortedWith(CaseInsensitiveComparator))
+
+}
