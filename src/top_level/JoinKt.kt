b7af04f5ef4a81921c59f2777632ffc915fcd257
better <zhaoyubetter@126.com>
2017-06-14 20:39:24 +0800

    添加测试代码

--- src/top_level/JoinKt.kt
+++ src/top_level/JoinKt.kt
@@ -69,3 +69,10 @@ fun main(args: Array<String>) {
     sb.lastChar = '!'
     println(sb)
 }
+
+// 扩展函数
+fun <T> MutableList<T>.swap(index1:Int, index2:Int) {
+    val tmp = this[index1]
+    this[index1] = this[index2]
+    this[index2] = tmp
+}
--- src/top_level/Top_Level_Test.java
+++ src/top_level/Top_Level_Test.java
@@ -1,4 +1,4 @@
-package test;
+package top_level;
 
 
 import java.util.ArrayList;
