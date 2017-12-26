9c65e2254ec22b3aeb99c8f98f02e93c7bf38c08
zhaoyu57 <zhaoyu1@jd.com>
2017-12-26 09:58:54 +0800

    扩展 - 在另一个类中，申明扩展

--- src/com/test/TestExtends.kt
+++ src/com/test/TestExtends.kt
@@ -1,13 +1,27 @@
 package com.test
-import com.better.extends_.betterSwap
-import com.better.extends_.lastIndex
+
+
 
 /**
  * Created by zhaoyu1 on 2017/12/14.
  */
+
+// 编译器常量
+const val SOLID = "1234"
+
+// 惰性初始化
+class Child(lateinit var num:String) {  // 不能用于构造函数
+    lateinit var name:String    // 惰性初始化
+    lateinit var age:Int        // 不能用 kotlin的原生类型
+    lateinit var num2:String?   // 不能非空类型
+
+    object Tmp {
+        const val A = "abc"
+    }
+}
+
 fun main(args: Array<String>) {
-    val list = mutableListOf("a","b","c")
-    list.betterSwap(1,2)
-    println(list.toString())
-    println(list.lastIndex)
+    val list = mutableListOf("a", "b", "c")
+    val c = Child()
+    println(c.name)
 }
