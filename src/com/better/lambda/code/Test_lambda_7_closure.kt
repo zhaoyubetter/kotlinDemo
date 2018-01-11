4bd309e1e505846d7c09f8ee466fc38da5fb39fb
zhaoyu57 <zhaoyu1@jd.com>
2018-01-11 09:32:58 +0800

    lambda 分享代码

--- src/com/better/lambda/code/Test_lambda_7_closure.kt
+++
@@ -1,8 +0,0 @@
-package com.better.lambda.code
-
-fun main(args: Array<String>) {
-    val list = listOf(1,2,3,4,5,6,7,8)
-    var oddNum = 0
-    list.forEach { if(it % 2== 0) {oddNum++} }
-    println("$oddNum")
-}
--- src/com/better/lambda/code/Test_lambda_8_extends_fun.kt
+++
@@ -1,6 +0,0 @@
-package com.better.lambda.code
-
-fun main(args: Array<String>) {
-    val sum1: Int.(Int) -> Int = fun Int.(other: Int) = this + other
-    println(1.sum1(22))
-}
