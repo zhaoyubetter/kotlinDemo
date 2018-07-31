50f3aa57306a2306e9dc4bd9ac53e82809dfe91d
zhaoyubetter <zhaoyubetter@126.com>
2018-07-31 09:50:21 +0800

    kotlin 操作符练习

--- src/main/kotlin/com/test/fn/FnTest6_operator3.kt
+++ src/main/kotlin/com/test/fn/FnTest6_operator3.kt
@@ -41,9 +41,21 @@ fun main(args: Array<String>) {
     // 9.dropWhile
     val dropWhile = array2.dropWhile { it < 0 }
     println("dropWhile: $dropWhile")
+
+    val list3 = listOf(1, 2, 3, -5, 0, -3, -2, 1)
+    val dropWhile2 = list3.dropWhile { it > 0 }
+    println("dropWhile2:$dropWhile2") // 输出 [-5, 0, -3, -2, 1]，看源码理解
     // 10.dropLastWhile 与 上类似
 
     // 11.slice
     println(array2.slice(listOf(0, 2, 4)))
     array2.sliceArray((0..2)).forEach { print(it) }
+
+    println()
+
+    // 12.distinct 去掉重复的
+    val list = listOf(-1, 2, 9, -20, 5, 3, -1, 2, 9, -9, 18, 9, -7, 4)
+    println("distinct: ${list.distinct()}")
+    println("distinct: ${list.distinctBy { it }}")
+
 }
